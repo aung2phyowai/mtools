b9161af409c293fb58ae05e0ff278fad843bbeda
Jakub Chromec <chromec.jakub@gmail.com>
2016-09-06 17:35:59 +0200

    .

--- startup.m
+++ startup.m
@@ -5,6 +5,7 @@
 %% Preferences
 startup_data = 1;             % Load project-related data from startup.mat
 add_external = 1;             % Add paths as provided by startup.mat
+add_local    = 1;             % Add subdirectories of the project dir to path
 show_welcome = 1;             % Show wd, Matlab version and host name
 restore_last = 1;             % Re-open files from last session
 tidy_up      = 1;             % Clear temporary variables created by this script
@@ -13,12 +14,8 @@ tidy_up      = 1;             % Clear temporary variables created by this script
 full_path = fileparts(mfilename('fullpath'));
 cd(full_path);
 
- % Add subdirs to path (and print a list)
-addpath(genpath(pwd));
-added = strcat({'Adding: '}, pwd, '/', cellpath(), '\n');
-fprintf([added{:}]);
-
 % Add mtools?
+addpath('m');
 
 %% Things that depend on Preferences
 
@@ -30,16 +27,25 @@ if startup_data
   project = load_project();
 end
 
-% if add_external && ~isempty(project)
-%   add_paths(project.paths)
-% end
+if add_external && ~isempty(project)
+  fprintf('\n');
+  paths = add_paths(project.paths);
+end
+
+if add_local
+  fprintf('\n');
+  addpath(genpath(pwd));
+  added = strcat({'Adding: '}, pwd, '/', cellpath(), '\n');
+  fprintf([added{:}]);
+end
 
 if restore_last && ~isempty(project)
+  fprintf('\n');
   session_restore(project.editor)
 end
 
 if tidy_up
   clear startup_data add_external show_welcome restore_last tidy_up
   clear full_path
-  clear project
+  clear added project
 end
