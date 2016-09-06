07d966e8d4605040597a97db854d210973db76f7
Jakub Chromec <chromec.jakub@gmail.com>
2016-09-06 09:54:57 +0200

    "."

--- m/exist_project.m
+++ m/exist_project.m
@@ -1,17 +1,49 @@
 %% Does project data-file created by mtools exist?
+%  status
+%  0    does not exist, needs to be created
+%  1    exists, can be loaded
+%  2    exists but invalid, will not be created
 
 function [status] = exist_project()
 
+% Is there 'startup.mat' ?
 try
-  load('startup.mat');
-  if ~strcmp(project.notice, 'Created by mtools')
-    error('Cannot find ''startup.mat'' created by mtools')
-  end
-  status = 1;
-  disp('Reading project file...');
-catch e
-  disp(e);
-  status = 0;
+    load('startup.mat');
+catch
+    disp('Cannot find ''startup.mat''')
+    status = 0;
+    return
+end
+
+% For now, check the validity of 'startup.mat' by testing class == struct
+% and fieldnames; but a proper class constructor is needed in the future
+expected_fields = {'name'; 'created'; 'opened'; 'paths'; 'editor'};
+
+% If 'startup.mat' exists, is it a struct with fields?
+try
+    loaded_fields = fieldnames(project);
+catch
+    disp('The file ''startup.mat'' is not a valid project structure');
+    status = 2;
+    return
+end
+
+% If so, do the fields have correct names?
+equal_length = length(loaded_fields) == length(expected_fields);
+equal_names  = all(strcmp(loaded_fields, expected_fields));
+
+if  equal_length && equal_names
+    disp('Found valid ''startup.mat''');
+    status = 1;
+else
+    msg = ['Found startup.mat but with incompatible fields\n\n' ...
+        'Should have:\n' ...
+        [sprintf('%s, ', expected_fields{1:end-1}), expected_fields{end}] ...
+        '\n\nbut has:\n' ...
+        [sprintf('%s, ', loaded_fields{1:end-1}), loaded_fields{end}] ...
+        '\n'];
+    fprintf(msg);
+    status = 2;
 end
 
 end
