09ac798fa186dad3edd3ef02f4d36aa80adbe89d
Jakub Chromec <chromec.jakub@gmail.com>
2016-09-06 09:54:49 +0200

    "."

--- m/initiate_project.m
+++ m/initiate_project.m
@@ -6,13 +6,13 @@ function [project] = initiate_project()
 paths = cell2table(cell(0,3), 'VariableNames', {'name', 'host', 'path'});
 
 project = struct;
+
 project.name = name;
 project.created = datetime(clock);
 project.opened = datetime(clock);
-project.editor = {};
-project.notice = 'Created by mtools';
 project.paths = paths;
+project.editor = {};
 
-save('project.mat', 'project')
+save('startup.mat', 'project')
 
 end
